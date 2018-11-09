ae1020424486eb76f63838d2a644d692be00c854
YauheniButski <ybutski@gmail.com>
2018-11-09 12:02:49 +0300

    1. Add Categories controller. 2. Correct Web.config to Allow 'Access Control Allow Origin'

---
+++ NitkaWebServer/NitkaWebServer/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace NitkaWebServer.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        // GET api/categories
+        public IEnumerable<Category> Get()
+        {
+            return new Category[] {
+                new Category() {Id = 1, Description = "TEST 1", DateCreate = DateTime.Now, Name = "TEST 1", Order = 1},
+                 new Category() {Id = 2, Description = "TEST 2", DateCreate = DateTime.Now, Name = "TEST 2", Order = 1},
+            };
+        }
+    }
+}
