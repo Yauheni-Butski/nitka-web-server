7f26d89ca74928fd232a18cb3ca5edd541af71f5
YauheniButski <ybutski@gmail.com>
2019-01-04 17:22:43 +0300

    1. Install System.Web.Cors Nuget package for enable CORS for client. 2. Add Login controller that can authenticate user.

--- NitkaWebServer/NitkaWebServer/App_Start/WebApiConfig.cs
+++ NitkaWebServer/NitkaWebServer/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace NitkaWebServer
 {
@@ -10,6 +11,9 @@ namespace NitkaWebServer
     {
         public static void Register(HttpConfiguration config)
         {
+            //TODO. Replace for real prod. origin
+            var corsAttr = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(corsAttr);
             // Web API configuration and services
             //Try to send as JSON format
             config.Formatters.JsonFormatter.SupportedMediaTypes
---
+++ NitkaWebServer/NitkaWebServer/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using NitkaWebServer.Models;
+using System.Web.Http;
+
+namespace NitkaWebServer.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost, Route("api/login")]
+        public bool Login(LoginViewModel model)
+        {
+            var isCanLogin = model.username == "admin" && model.password == "admin";
+            return isCanLogin; //return false
+        }
+    }
+}
