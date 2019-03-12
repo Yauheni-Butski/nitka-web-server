5f4330f026e310c301eb27879fe0fc0288f732aa
YauheniButski <ybutski@gmail.com>
2019-03-12 12:53:52 +0300

    Add Like stats to ImagePlate model.

--- NitkaWebServer/NitkaWebServer/Models/ImagePlate.cs
+++ NitkaWebServer/NitkaWebServer/Models/ImagePlate.cs
@@ -5,12 +5,16 @@
         public int id { get; set; }
         public string thumbPath { get; set; }
         public string imagePath { get; set; }
+        public int likeCount { get; set; }
+        public bool isUserLike { get; set; }
 
-        public ImagePlate(int id, string thumbPath, string imagePath)
+        public ImagePlate(int id, string thumbPath, string imagePath, int likeCount, bool isUserLike)
         {
             this.id = id;
             this.thumbPath = thumbPath;
             this.imagePath = imagePath;
+            this.likeCount = likeCount;
+            this.isUserLike = isUserLike;
         }
     }
 }
